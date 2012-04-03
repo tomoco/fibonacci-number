0dc05dfc5251b3baf1327a8b0c444299e242b4bb
unknown <tomoco@4101201-PC.morijyobi.intra>
2012-04-03 10:25:38 +0900

    first commit

---
+++ fib.js
@@ -0,0 +1,17 @@
+﻿var month = 0;
+var number_of_pairs = 1;
+var before_number_of_pairs = 0;
+
+while( month < 12 ){
+
+	var b = before_number_of_pairs;
+
+	number_of_pairs = number_of_pairs + before_number_of_pairs;
+	before_number_of_pairs = b;
+		
+	month = month + 1;
+	
+	console.log("month: " + month)
+	console.log("pairs:" + number_of_pairs);
+	console.log("b_pairs:" + before_number_of_pairs);
+}
